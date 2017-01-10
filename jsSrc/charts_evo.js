66d1dd4e0d302693ea14739b0d2df72b46a1a777
Giorgio Mula <giorgio_mula@hotmail.com>
2017-01-10 17:46:57 +0100

    Import Eclipse JDT project starting draft.

---
+++ jsSrc/charts_evo.js
@@ -0,0 +1,196 @@
+/**
+ * 
+ */
+
+function Person(aName, imgPath, aRole)
+{
+	this.name = aName;
+	this.img = imgPath;
+	this.role = aRole;
+	this.manager = null;
+	this.childArray = new Array();
+
+	this.addChild = function(refPerson)
+	{
+		this.childArray.push(refPerson);
+	};
+
+	this.addManager = function(refManager)
+	{
+		this.manager = refManager;
+	};
+}
+
+function PersonMap()
+{
+	this.addPerson = function(refPerson, personManagerName)
+	{
+		writeDebug("Add person " + refPerson.name);
+		this[refPerson.name] = refPerson;
+		if (personManagerName == "")
+		{
+			this.CEO = refPerson;
+			writeDebug(refPerson.name + " is the CEO !");
+		}
+		else
+		{
+			var managerObject = this[personManagerName];
+			refPerson.addManager(managerObject);
+			managerObject.addChild(refPerson);
+			writeDebug("Added " + managerObject.name + " as " + refPerson.name
+					+ " manager");
+		}
+	};
+
+	this.getPerson = function(personName)
+	{
+		return this[personName];
+	};
+}
+
+var personMap = new PersonMap();
+var levelNodesArray = new Array();
+var levelIndex = 0;
+var rootElement;
+
+/**
+ * Attach a new DOM element to parent. The DOM element is a "div" represent the
+ * employee with class="person" and id=person's name
+ * 
+ * @param person
+ *          the person data
+ * @param group
+ *          the DOM parent node
+ */
+function createHTMLPersonInGroup(person, group)
+{
+	var newNode = document.createElement("div");
+	newNode.setAttribute("class", "person");
+	newNode.setAttribute("id", person.name);
+	var newContent = '<img src="../test/' + person.img + '" alt="' + person.name
+			+ '" />';
+	newContent += '<p><strong>' + person.name + '</strong><br/>';
+	newContent += person.role + '</p>';
+	newNode.innerHTML = newContent;
+	group.appendChild(newNode);
+}
+
+function createHTMLGroupInLevel(manager, level)
+{
+	var newNode = document.createElement("div");
+	newNode.setAttribute("class", "group");
+	newNode.setAttribute("owner", manager == null ? "CEO" : manager.name);
+	var newContent = '<svg></svg>'; // TODO: connecting lines, after all person
+	// added
+	newNode.innerHTML = newContent;
+	level.appendChild(newNode);
+	return newNode;
+}
+
+/**
+ * Add a new <div> level to levelNodesArray global array
+ * 
+ * @returns reference to <div> element representing an inline-block styled level
+ */
+function createNewLevel()
+{
+	var newLevel = document.createElement("div");
+	newLevel.setAttribute("class", "level");
+	rootElement.appendChild(newLevel);
+	writeDebug("Create level " + levelNodesArray.length);
+	levelNodesArray.push(newLevel);
+	return newLevel;
+}
+
+function writeDiagram()
+{
+	rootElement = document.getElementById("org-chart");
+
+	var person = personMap.CEO;
+	writeBranch(person, 0);
+}
+
+function writeBranch(employee, levelIdx)
+{
+	var level = levelNodesArray[levelIdx];
+	if (level == null)
+	{
+		level = createNewLevel();
+	}
+	// var group = createHTMLGroupInLevel(employee.manager, level);
+	var group = null;
+	var groups = level.getElementsByTagName("div");
+	for (var groupIndex = 0; groupIndex < groups.length; groupIndex++)
+	{
+		if (groups[groupIndex].getAttribute("owner") == employee.manager.name)
+		{
+			group = groups[groupIndex];
+			break;
+		}
+	}
+	if (group == null)
+	{
+		group = createHTMLGroupInLevel(employee.manager, level);
+	}
+	
+	createHTMLPersonInGroup(employee, group);
+	for (var idxChild = 0; idxChild < employee.childArray.length; idxChild++)
+	{
+		writeBranch(employee.childArray[idxChild], levelIdx + 1);
+	}
+}
+
+function readSingleFile(e)
+{
+	var file = e.target.files[0];
+	if (!file)
+	{
+		return;
+	}
+	var reader = new FileReader();
+	reader.onload = function(e)
+	{
+		var contents = e.target.result;
+		var textLines = contents.split("\n");
+
+		writeDebug("", true);
+		textLines.forEach(parseCsvLine);
+		writeDiagram();
+	};
+	reader.readAsText(file);
+}
+
+function parseCsvLine(textLine, index)
+{
+	if (textLine.length == 0 || textLine[0] == '#' || textLine[0] == '\n'
+			|| textLine[0] == '\r')
+	{
+		return;
+	}
+	var strArray = textLine.split(",", 4);
+	if (strArray.length != 4)
+	{
+		alert("File bad format on line " + index);
+		return;
+	}
+
+	var employee = new Person(strArray[0].trim(), strArray[3].trim(), strArray[1]
+			.trim());// , strArray[2].trim());
+	personMap.addPerson(employee, strArray[2].trim());
+	writeDebug("personMap now is " + personMap);
+
+	/*
+	 * if (employee.role.search("Manager") != -1) { chartArray[0].push(employee); }
+	 * else { chartArray[1].push(employee); }
+	 */
+}
+
+function writeDebug(text, clear)
+{
+	var dbgElement = document.getElementById("debug");
+	if (clear)
+	{
+		dbgElement.innerHTML = "";
+	}
+	dbgElement.innerHTML += text + '<br/>';
+}
